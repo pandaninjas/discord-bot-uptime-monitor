7b9f332aac48b7a13281d727911f6feda35ecf66
pandaninjas <101084582+pandaninjas@users.noreply.github.com>
2022-09-23 09:48:30 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,38 @@
 const guild_widget_url = ""; // replace the guild_widget_url here;
-const replit_db = "https://kv.replit.com/v0/<token here>"; // replace the replit db url here
-const bot_username = "Bot"; // replace bot username here
+const replit_url = ""; // replace the repl url here
+const bot_username = ""; // replace bot username here
+const key = ""; // replace key used here
+
+const base64ToUint8 = (str) => Uint8Array.from(atob(str.trim()), (c) => c.charCodeAt(0));
+const encode = new TextEncoder();
+const decode = new TextDecoder();
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
+
 addEventListener("scheduled", (event) => {
   event.waitUntil(
     (async () => {
+      let result = await fetch(
+        replit_url
+      ).then((res) => res.text());
+      let parts = result.split("$");
+      let iv = base64ToUint8(parts[0]);
+      let enc = base64ToUint8(parts[1]);
+      const replit_db = decode.decode(new Uint8Array(await crypto.subtle.decrypt(
+        {
+          name: "AES-CBC",
+          iv: iv
+        },
+        await crypto.subtle.importKey(
+          "raw",
+          encode.encode(key),
+          "AES-CBC",
+          false,
+          ["decrypt"]
+        ),
+        enc
+      )))
       let b = await fetch(
         guild_widget_url
       ).then((res) => res.json());
@@ -35,6 +61,27 @@ addEventListener("scheduled", (event) => {
 });
 
 async function handleRequest(request) {
+  let result = await fetch(
+    replit_url
+  ).then((res) => res.text());
+  let parts = result.split("$");
+  let iv = base64ToUint8(parts[0]);
+  let enc = base64ToUint8(parts[1]);
+  const replit_db = decode.decode(new Uint8Array(await crypto.subtle.decrypt(
+    {
+      name: "AES-CBC",
+      iv: iv
+    },
+    await crypto.subtle.importKey(
+      "raw",
+      encode.encode(key),
+      "AES-CBC",
+      false,
+      ["decrypt"]
+    ),
+    enc
+  )))
+
   let offline = await fetch(replit_db + "/fail").then((res) => res.text());
   offline = parseInt(offline);
   let online = await fetch(replit_db + "/success").then((res) => res.text());
